eaf59bf8ecdcb9d6421a64b7f7560c6a2cbb5bc0
saidinesh49 <143427352+saidinesh49@users.noreply.github.com>
2024-05-03 18:59:13 +0530

    Delete AllUseFunctions.js

--- AllUseFunctions.js
+++
@@ -1,7 +0,0 @@
-function Logoutuser(){
-    alert("Successfully Logout!");
-    localStorage.clear();
-    window.location.replace('http://127.0.0.1:5500/Login.html');
-   }
-
-export { Logoutuser };
